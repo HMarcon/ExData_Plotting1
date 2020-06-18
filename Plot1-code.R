28920c9e6a4ee5be191c2348882acde422c677dc
HMarcon <henriquemmagri42@gmail.com>
2020-06-18 17:11:45 -0300

    Plot and histogram created

--- Plot1-code.R
+++ Plot1-code.R
@@ -10,3 +10,10 @@ data$dateTime <- strptime(data$dateTime, "%d/%m/%Y %H:%M:%S")
 
 #Subsetting! 
 data_subset <- data[data$Date %in% c("1/2/2007", "2/2/2007"),]
+
+#Create plot
+png("plot1.png", width=480, height=480, units="px")
+
+#Create histogram
+hist(data_subset$Global_active_power, col="red", main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)")
